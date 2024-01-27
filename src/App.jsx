97bb4f75fe475966e491cab0746b4b7277075912
Sam Peck <fullstacksam@protonmail.com>
2024-01-26 23:37:25 -0500

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import './App.css'
-import NavBar from './NavBar' 
+import HomePage from './HomePage/HomePage';
 
 function App() {
 
   return (
    <div className="App-Wrapper">
-    <NavBar />
-    <div className="Wrapper-Title">
-      <h1 className="Home-Title">Samdn</h1>
-      <h4>Sam's version of MDN</h4>
-    </div>
-    <p id="Intro">Information overload? I'm keeping my notes here</p>
+      <HomePage />
    </div>
   )
 }
